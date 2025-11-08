08400bc0d3ac2ff0c15df1a6a576d96b81719f9d
ksheth-ontic <ksheth@ontic.co>
2025-11-08 18:20:49 +0530

    [First][Commit] USE-0000 first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author kushalsheth
+ * @since 08/11/25
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
